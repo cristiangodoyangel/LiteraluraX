0df3f4b81deb2f98b3b5965cef2d72b64b6b0504
Star <creacciones@gmail.com>
2024-12-21 23:41:01 -0300

    - Se inicia proyecto

---
+++ src/main/java/com/aluracursos/desafio/LiteraluraX/LiteraluraXApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio.LiteraluraX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/desafio/LiteraluraX/LiteraluraXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio.LiteraluraX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
